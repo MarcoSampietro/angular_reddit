625e138c130821ea201b1a97429067d3f9008366
Marco Sampietro <marco.sampietro1574@gmail.com>
2025-05-21 11:55:01 +0000

    aggiunto adding new articles

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import Article from './article/app.model';
+import { ArticleComponent } from './article/article.component';
 
 @Component({
   selector: 'app-root',
-  imports: [Article],
+  imports: [ArticleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    let t = title.value;
-    let v= link.value;
-    console.log(`Adding article title: `+t+ ` and link: `+ v);
-    console.log(`Adding article title: `+title.value+ ` and link: `+ link.value);
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    return false;
+  articles: any;
+  addArticle(title: string, link: string): void {
+    console.log(`Adding article title: ${title} and link: ${link}`);
+    this.articles.push(new Article(title, link, 0));
+  }
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
--- angular-reddit/src/app/article/app.model.ts
+++ angular-reddit/src/app/article/app.model.ts
@@ -19,12 +19,13 @@ export default class Article {
   
     // domain() is a utility function that extracts
     // the domain from a URL, which we'll explain shortly
-    domain(): string {
-      try {// e.g. http://foo.com/path/to/bar
-        const domainAndPath: string = this.link.split('//')[1]; // e.g. foo.com/path/to/bar
-        return domainAndPath.split('/')[0];
-      } catch (err) {
-        return 'null';
-      }
-    }
+    domain(): string | null {
+        try {
+          const url = new URL(this.link);
+          return url.hostname; // e.g. "foo.com"
+        } catch (error) {
+          console.error('Invalid URL:', this.link);
+          return null;
+        }
+      }      
 }
